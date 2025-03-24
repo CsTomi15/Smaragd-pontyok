a0544730f93279629e9f6968a56ac1901b5a6ea8
csoki1234567 <csoki654321@gmail.com>
2025-03-24 19:55:35 +0100

    jolvan dave

--- ac#/sim.py
+++ ac#/sim.py
@@ -1,3 +1,30 @@
-x = open(r"D:\ori\jaja ismered a sim\Smaragd-pontyok\ac#\verseny.csv","r", encoding="UTF-8")
+import csv
+#epuletek = open(r"C:\Users\orosz\Desktop\Smaragd-pontyok\ac#\verseny.csv","r", encoding="UTF-8")
 
-print(x.read())
+#print(epuletek.read())
+'''
+class Epulet:
+    def __init__(self,azonosito,nev,tipus,epitesiEve,hasznosTerulet):
+        self.azonosito = int(azonosito)
+        self.nev = nev
+        self.tipus = tipus
+        self.epitesiEve = int(epitesiEve)
+        self.hasznosTerulet = int(hasznosTerulet)
+'''
+
+
+def csv_to_dict_list(fajlnev):
+    with open(fajlnev, newline='', encoding='utf-8') as file:
+        olvaso = csv.DictReader(file, delimiter=";")
+        return [sor for sor in olvaso]
+
+# Betöltés
+epuletek = csv_to_dict_list(r"C:\Users\orosz\Desktop\Smaragd-pontyok\ac#\verseny.csv")
+
+# Kiírás
+for epulet in epuletek:
+    print(epulet)
+
+
+
+
